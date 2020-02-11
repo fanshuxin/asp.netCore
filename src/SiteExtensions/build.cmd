2909289c848e6112733a7f4afb9f750ce62fc5a6
Will Godbe <wigodbe@microsoft.com>
2020-02-11 14:02:03 -0800

    Test run of separating build legs

--- src/SiteExtensions/build.cmd
+++ src/SiteExtensions/build.cmd
@@ -1,9 +1,19 @@
 @ECHO OFF
 SET RepoRoot=%~dp0..\..
 
+:: Set the default arguments for build
+SET __BuildArch=x64
+
+:Arg_Loop
+if "%1" == "" goto ArgsDone
+
+if /i "%1" == "-x64"                 (set __BuildArch=x64&shift&goto ArgsDone)
+if /i "%1" == "-x86"                 (set __BuildArch=x86&shift&goto ArgsDone)
+
+:ArgsDone
+
 ECHO Building Microsoft.AspNetCore.Runtime.SiteExtension
-CALL %RepoRoot%\build.cmd -arch x64 -projects %~dp0Runtime\Microsoft.AspNetCore.Runtime.SiteExtension.pkgproj /bl:artifacts/log/SiteExtensions-Runtime-x64.binlog %*
-CALL %RepoRoot%\build.cmd -arch x86 -projects %~dp0Runtime\Microsoft.AspNetCore.Runtime.SiteExtension.pkgproj /bl:artifacts/log/SiteExtensions-Runtime-x86.binlog %*
+CALL %RepoRoot%\build.cmd -arch %__BuildArch% -projects %~dp0Runtime\Microsoft.AspNetCore.Runtime.SiteExtension.pkgproj /bl:artifacts/log/SiteExtensions-Runtime-%__BuildArch%.binlog %*
 
 IF %ERRORLEVEL% NEQ 0 (
    EXIT /b %ErrorLevel%
@@ -11,15 +21,16 @@ IF %ERRORLEVEL% NEQ 0 (
 
 ECHO Building LoggingBranch
 REM /p:DisableTransitiveFrameworkReferences=true is needed to prevent SDK from picking up transitive references to Microsoft.AspNetCore.App as framework references https://github.com/dotnet/sdk/pull/3221
-CALL %RepoRoot%\build.cmd -forceCoreMsbuild -arch x64 -projects %~dp0LoggingBranch\LB.csproj /p:DisableTransitiveFrameworkReferences=true /bl:artifacts/log/SiteExtensions-LoggingBranch-x64.binlog %*
-CALL %RepoRoot%\build.cmd -forceCoreMsbuild -arch x86 -projects %~dp0LoggingBranch\LB.csproj /p:DisableTransitiveFrameworkReferences=true /bl:artifacts/log/SiteExtensions-LoggingBranch-x86.binlog %*
+CALL %RepoRoot%\build.cmd -forceCoreMsbuild -arch %__BuildArch% -projects %~dp0LoggingBranch\LB.csproj /p:DisableTransitiveFrameworkReferences=true /bl:artifacts/log/SiteExtensions-LoggingBranch-%__BuildArch%.binlog %*
 
 IF %ERRORLEVEL% NEQ 0 (
    EXIT /b %ErrorLevel%
 )
 
-ECHO Building Microsoft.AspNetCore.AzureAppServices.SiteExtension
-CALL %RepoRoot%\build.cmd -forceCoreMsbuild -projects %~dp0LoggingAggregate\src\Microsoft.AspNetCore.AzureAppServices.SiteExtension\Microsoft.AspNetCore.AzureAppServices.SiteExtension.csproj /bl:artifacts/log/SiteExtensions-LoggingAggregate.binlog %*
+IF "%__BuildArch%"=="x64" (
+	ECHO Building Microsoft.AspNetCore.AzureAppServices.SiteExtension
+	CALL %RepoRoot%\build.cmd -forceCoreMsbuild -projects %~dp0LoggingAggregate\src\Microsoft.AspNetCore.AzureAppServices.SiteExtension\Microsoft.AspNetCore.AzureAppServices.SiteExtension.csproj /bl:artifacts/log/SiteExtensions-LoggingAggregate.binlog %*
+)
 
 IF %ERRORLEVEL% NEQ 0 (
    EXIT /b %ErrorLevel%
